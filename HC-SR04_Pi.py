ee68412642e232d89f2cb2fb6d481541831297c8
Kaan Öztüzün <kaanoztuzun@gmail.com>
2024-06-02 10:08:27 +0300

    Update HC-SR04_Pi.py

--- HC-SR04_Pi.py
+++ HC-SR04_Pi.py
@@ -1,22 +1,18 @@
-# Getting the libraries we need
 from gpiozero import DistanceSensor
 from time import sleep
 
 # Initialize ultrasonic sensor
-sensor = DistanceSensor(trigger=18, echo=24)
+sensor = DistanceSensor(trigger=23, echo=24)
 
 while True:
-	# Wait 2 seconds
-	sleep(2)
-	
-	# Get the distance in metres
-	distance = sensor.distance
+    # Wait 2 seconds
+    sleep(2)
 
-	# But we want it in centimetres
-	distance = sensor.distance * 100
+    # Get the distance in metres
+    distance = sensor.distance
 
-	# We would get a large decimal number so we will round it to 2 places
-	distance = round(sensor.distance, 2)
+    # Convert to centimeters and round to 2 decimal places
+    distance_cm = round(distance * 100, 2)
 
-	# Print the information to the screen
-	print("Distance: {} cm".format(sensor.distance))
+    # Print the information to the screen
+    print("Distance: {} cm".format(distance_cm))
