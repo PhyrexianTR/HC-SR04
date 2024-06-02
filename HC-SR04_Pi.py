cbd226a258b874e4ab6fde5af0a83f5e67c9a51a
Kaan Öztüzün <kaanoztuzun@gmail.com>
2024-06-02 09:39:39 +0300

    Update HC-SR04_Pi.py

--- HC-SR04_Pi.py
+++ HC-SR04_Pi.py
@@ -1,47 +1,38 @@
 import RPi.GPIO as GPIO
 import time
-
-# GPIO pin ayarları
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
 TRIG = 23
 ECHO = 24
 
-# Pin modlarını belirleme
-GPIO.setup(TRIG, GPIO.OUT)
-GPIO.setup(ECHO, GPIO.IN)
+print "HC-SR04 mesafe sensoru"
 
-def measure_distance():
-    # Trig pinini düşük yaparak başlat
-    GPIO.output(TRIG, False)
-    time.sleep(2)
-    
-    # Trig pinini yüksek yap ve kısa bir süre sonra tekrar düşük yap
-    GPIO.output(TRIG, True)
-    time.sleep(0.00001)
-    GPIO.output(TRIG, False)
+GPIO.setup(TRIG,GPIO.OUT)
+GPIO.setup(ECHO,GPIO.IN)
 
-    # Echo pininden gelen sinyali ölçme
-    while GPIO.input(ECHO) == 0:
-        start_time = time.time()
+while True:
 
-    while GPIO.input(ECHO) == 1:
-        stop_time = time.time()
+  GPIO.output(TRIG, False)
+  print "Olculuyor..."
+  time.sleep(2)
 
-    # Zaman farkını ve mesafeyi hesaplama
-    time_elapsed = stop_time - start_time
-    distance = (time_elapsed * 34300) / 2
+  GPIO.output(TRIG, True)
+  time.sleep(0.00001)
+  GPIO.output(TRIG, False)
 
-    return distance
+  while GPIO.input(ECHO)==0:
+    pulse_start = time.time()
 
-try:
-    while True:
-        dist = measure_distance()
-        if dist:
-            print(f"Distance: {dist:.2f} cm")
-        else:
-            print("No valid distance measured.")
-        time.sleep(1)
+  while GPIO.input(ECHO)==1:
+    pulse_end = time.time()
 
-except KeyboardInterrupt:
-    print("Measurement stopped by User")
-    GPIO.cleanup()
+  pulse_duration = pulse_end - pulse_start
+
+  distance = pulse_duration * 17150
+  distance = round(distance, 2)
+
+  if distance > 2 and distance < 400:
+    print "Mesafe:",distance - 0.5,"cm"
+  else:
+    print "Menzil asildi"
