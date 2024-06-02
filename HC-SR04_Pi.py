ec2b3a00129374ad5da47d3ccada28abc93c486f
Kaan Öztüzün <kaanoztuzun@gmail.com>
2024-06-02 08:30:50 +0300

    Update

--- HC-SR04_Pi.py
+++ HC-SR04_Pi.py
@@ -1,44 +1,56 @@
 import RPi.GPIO as GPIO
 import time
- 
+
 # GPIO ayarları
 GPIO.setmode(GPIO.BCM)
 TRIG = 23
 ECHO = 24
- 
+
 GPIO.setup(TRIG, GPIO.OUT)
 GPIO.setup(ECHO, GPIO.IN)
- 
+
 def measure_distance():
     # Trig pinini düşük yap ve bekle
     GPIO.output(TRIG, False)
     time.sleep(2)
- 
+    
     # Trig pinini yüksek yap ve kısa bir süre sonra tekrar düşük yap
     GPIO.output(TRIG, True)
     time.sleep(0.00001)
     GPIO.output(TRIG, False)
- 
+
     # Echo pininden gelen sinyali ölç
     while GPIO.input(ECHO) == 0:
         pulse_start = time.time()
- 
+        start_signal_received = True
+
+    if not start_signal_received:
+        print("Echo pin did not receive the start signal.")
+        return None
+
     while GPIO.input(ECHO) == 1:
         pulse_end = time.time()
- 
+        end_signal_received = True
+
+    if not end_signal_received:
+        print("Echo pin did not receive the end signal.")
+        return None
+
     pulse_duration = pulse_end - pulse_start
- 
+
     # Mesafeyi hesapla
     distance = pulse_duration * 17150
     distance = round(distance, 2)
     return distance
- 
+
 try:
     while True:
         dist = measure_distance()
-        print("Distance: {} cm".format(dist))
+        if dist is not None:
+            print("Distance: {} cm".format(dist))
+        else:
+            print("No valid distance measured.")
         time.sleep(1)
- 
+
 except KeyboardInterrupt:
     GPIO.cleanup()
- 
