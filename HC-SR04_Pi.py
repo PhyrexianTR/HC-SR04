f84b7ec8668d7d01eb72a461bcda3b7e97e6ce58
Kaan Öztüzün <kaanoztuzun@gmail.com>
2024-06-02 10:06:43 +0300

    Update HC-SR04_Pi.py

--- HC-SR04_Pi.py
+++ HC-SR04_Pi.py
@@ -1,70 +1,22 @@
-import RPi.GPIO as GPIO
-import time
+# Getting the libraries we need
+from gpiozero import DistanceSensor
+from time import sleep
 
-# GPIO ayarları
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
+# Initialize ultrasonic sensor
+sensor = DistanceSensor(trigger=18, echo=24)
 
-TRIG = 23
-ECHO = 24
+while True:
+	# Wait 2 seconds
+	sleep(2)
+	
+	# Get the distance in metres
+	distance = sensor.distance
 
-print("HC-SR04 mesafe sensörü")
+	# But we want it in centimetres
+	distance = sensor.distance * 100
 
-GPIO.setup(TRIG, GPIO.OUT)
-GPIO.setup(ECHO, GPIO.IN)
+	# We would get a large decimal number so we will round it to 2 places
+	distance = round(sensor.distance, 2)
 
-def measure_distance():
-    # Trig pinini düşük yaparak başlat
-    GPIO.output(TRIG, False)
-    time.sleep(2)
-    
-    # Trig pinini yüksek yap ve kısa bir süre sonra tekrar düşük yap
-    GPIO.output(TRIG, True)
-    time.sleep(0.00001)
-    GPIO.output(TRIG, False)
-
-    # Echo pininden gelen sinyali ölçme
-    start_time = time.time()
-    stop_time = time.time()
-
-    timeout = start_time + 0.05  # 50 ms timeout
-
-    while GPIO.input(ECHO) == 0 and time.time() < timeout:
-        start_time = time.time()
-
-    if time.time() >= timeout:
-        print("Echo pin did not receive the start signal.")
-        return None
-
-    timeout = time.time() + 0.05  # 50 ms timeout
-
-    while GPIO.input(ECHO) == 1 and time.time() < timeout:
-        stop_time = time.time()
-
-    if time.time() >= timeout:
-        print("Echo pin did not receive the end signal.")
-        return None
-
-    # Zaman farkını ve mesafeyi hesaplama
-    time_elapsed = stop_time - start_time
-    distance = (time_elapsed * 34300) / 2
-    distance = round(distance, 2)
-
-    return distance
-
-try:
-    while True:
-        print("Ölçülüyor...")
-        dist = measure_distance()
-        if dist is not None:
-            if 2 < dist < 400:
-                print("Mesafe:", dist - 0.5, "cm")
-            else:
-                print("Menzil aşıldı")
-        else:
-            print("Geçerli bir mesafe ölçülemedi.")
-        time.sleep(1)
-
-except KeyboardInterrupt:
-    print("Measurement stopped by User")
-    GPIO.cleanup()
+	# Print the information to the screen
+	print("Distance: {} cm".format(sensor.distance))
